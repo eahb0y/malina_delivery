024d09bf3941399ed214e38f95107e77548bbf49
iskandar <iskandarmister323@gmail.com>
2024-11-10 20:00:28 +0500

    main tab pages added

---
+++ lib/route/name_routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String initial = "/";
+  static const String home = "/home";
+  static const String main = '/main';
+  static const String basket = "/basket";
+  static const String profile = "/profile";
+  static const String qrScan = "/qrScan";
+  static const String favourite = "/favourite";
+}
